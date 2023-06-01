3b138c0a879c0ac03a2c38c35621176e9446a274
ssomy3800 <ssomy3800@gmail.com>
2023-06-01 05:12:45 -0700

    bootstrap

--- frontend/src/component/Navigation/ProfileButton.js
+++ frontend/src/component/Navigation/ProfileButton.js
@@ -1,29 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { Dropdown } from "react-bootstrap";
 import * as userActions from "../../store/usersreducer";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import { Dropdown } from 'react-bootstrap';
 
 function ProfileButton({ user }) {
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = () => {
-      setShowMenu(false);
-    };
-
-    document.addEventListener("click", closeMenu);
-
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+  const handleOpen = () => setOpen(!open);
+  const handleClose = () => setOpen(false);
 
   const logout = (e) => {
     e.preventDefault();
@@ -31,19 +16,20 @@ function ProfileButton({ user }) {
   };
 
   return (
-    <div className="dropdown-menu">
-      <button className="user-profile-button" onClick={openMenu}>
-        <i className="fa-solid fa-user-circle" />
-        {showMenu && (
-          <ul className="dropdown-item">
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li>
-              <button onClick={logout}>Log Out</button>
-            </li>
-          </ul>
+    <div onMouseEnter={handleOpen} onMouseLeave={handleClose}>
+      <Dropdown>
+        <Dropdown.Toggle id="dropdown-basic" as="div">
+          <i className="fa-solid fa-user-circle" />
+        </Dropdown.Toggle>
+        {open && (
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">{user.username}</Dropdown.Item>
+            <Dropdown.Item href="#">{user.email}</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={logout}>Log Out</Dropdown.Item>
+          </Dropdown.Menu>
         )}
-      </button>
+      </Dropdown>
     </div>
   );
 }
