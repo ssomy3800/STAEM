4ea23a089ce63d80a1da3e6a925f7b0587867a47
ssomy3800 <ssomy3800@gmail.com>
2023-06-05 00:16:59 -0700

    part of gamepage done

--- frontend/src/component/gamePage/index.js
+++ frontend/src/component/gamePage/index.js
@@ -2,6 +2,10 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchGame } from "../../store/gamesreducer";
+import { Carousel as ResponsiveCarousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./gamePage.css";
+import SearchBar from "../searchBar";
 
 function GamePage() {
   const { id } = useParams();
@@ -12,27 +16,58 @@ function GamePage() {
     dispatch(fetchGame(id));
   }, [dispatch, id]);
 
-  // If the game data has not yet been fetched, render a loading message
   if (!game) {
     return <div>Loading...</div>;
   }
 
-  // Render the game data
+  const gameImages = Object.entries(game.images).map(
+    ([filename, imageUrl], index) => (
+      <div key={index}>
+        <img className="carousel-image" src={imageUrl} alt={filename} />
+      </div>
+    )
+  );
+  const firstImageUrl = game.images[Object.keys(game.images)[0]];
+
   return (
-    <div>
-      <h2>{game.title}</h2>
-      <p>Publisher: {game.publisher}</p>
-      <p>Short Description: {game.shortDescription}</p>
-      <p>Long Description: {game.longDescription}</p>
-      {Object.entries(game.images).map(([filename, imageUrl], index) => (
-        <img key={index} src={imageUrl} alt={filename} />
-      ))}
-      {game.video && (
-        <video src={game.video} controls>
-          Your browser does not support the video tag.
-        </video>
-      )}
-    </div>
+    <>
+      <SearchBar />
+      <div className="game-page">
+        <div className="game-carousel">
+          <ResponsiveCarousel
+            showThumbs={true}
+            autoPlay
+            infiniteLoop
+            useKeyboardArrows
+          >
+            {game.video && (
+              <div>
+                <video
+                  poster={game.videoPreview}
+                  className="carousel-image"
+                  src={game.video}
+                  controls
+                >
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            )}
+            {gameImages}
+          </ResponsiveCarousel>
+        </div>
+        <div className="rightside">
+        <img src={firstImageUrl} alt="Game Cover" className="game-cover" />
+        <div className="game-info">
+          <h2>{game.title}</h2>
+          <p>Publisher: {game.publisher}</p>
+          <p>Developer: {game.developer}</p>
+          <p>Publish Date: {game.publishDate}</p>
+          <p>Short Description: {game.shortDescription}</p>
+          <p>Long Description: {game.longDescription}</p>
+        </div>
+        </div>
+      </div>
+    </>
   );
 }
 
